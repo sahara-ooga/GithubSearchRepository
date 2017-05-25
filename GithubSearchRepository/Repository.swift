9fb7899d91b62f2fe57b01409328ea7dbe13da93
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-25 15:31:53 +0900

    Repository型の定義

---
+++ GithubSearchRepository/Repository.swift
@@ -0,0 +1,16 @@
+//
+//  Repository.swift
+//  GithubSearchRepository
+//
+//  Created by yuu ogasawara on 2017/05/25.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+import Foundation
+
+struct Repository{
+    let id:Int
+    let name:String
+    let fullName:String
+    let owner:User
+}
