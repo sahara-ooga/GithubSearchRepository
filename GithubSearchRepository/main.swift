35a24993af4282133818d3841584ac7cdf88a172
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-25 15:15:24 +0900

    Initial Commit

---
+++ GithubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GithubSearchRepository
+//
+//  Created by yuu ogasawara on 2017/05/25.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
