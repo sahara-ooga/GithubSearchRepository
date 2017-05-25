97fff83809409ff7fc80e6b90d129246c7e2019a
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-25 15:21:40 +0900

    User型の定義

---
+++ GithubSearchRepository/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  GithubSearchRepository
+//
+//  Created by yuu ogasawara on 2017/05/25.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let id:Int
+    let login:String
+}
+
