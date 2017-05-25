8417ae2066cd17098a3cba309f22162a0a31bf98
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-25 17:38:32 +0900

    リクエストのプロトコルを定義

---
+++ GithubSearchRepository/GitHubRequest.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubRequest.swift
+//  GithubSearchRepository
+//
+//  Created by yuu ogasawara on 2017/05/25.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+import Foundation
+
+protocol GitHubRequest {
+    var baseURL:URL { get }
+    var path:String { get }
+}
+
+extension GitHubRequest{
+    var baseURL:URL {
+        return URL(string: "https://api.github.com")!
+    }
+}
