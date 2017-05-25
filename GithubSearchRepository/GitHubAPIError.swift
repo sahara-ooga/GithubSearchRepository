a4da34815265b0774fbe99797f129edcb7a9d430
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-25 17:32:35 +0900

    レスポンス中のエラーの配列を扱うプロパティ・構造体を定義

--- GithubSearchRepository/GitHubAPIError.swift
+++ GithubSearchRepository/GitHubAPIError.swift
@@ -8,6 +8,7 @@
 
 struct GitHubAPIError:Error,JSONDecodable {
     let message:String
+    let fieldErrors:[FieldError]
     
     init(json:Any)throws {
         guard let dictionary = json as? [String:Any] else {
@@ -19,6 +20,50 @@ struct GitHubAPIError:Error,JSONDecodable {
                                                actualValue: dictionary["message"])
         }
         
+        let fieldErrorObjects = dictionary["errors"] as? [Any] ?? []
+        
+        let fieldErrors = try fieldErrorObjects.map{
+            return try FieldError(json: $0)
+        }
+        
         self.message = message
+        self.fieldErrors = fieldErrors
+    }
+}
+
+extension GitHubAPIError{
+    struct FieldError {
+        let resource:String
+        let field:String
+        let code:String
+        
+        init(json:Any) throws {
+            
+            guard let dictionary = json as? [String : Any] else {
+                throw JSONDecodeError.invalidFormat(json: json)
+            }
+            
+            guard let resource = dictionary["resource"] as? String else {
+                throw JSONDecodeError.missingValue(
+                    key: "resource",
+                    actualValue: dictionary["resource"])
+            }
+            
+            guard let field = dictionary["field"] as? String else {
+                throw JSONDecodeError.missingValue(
+                    key: "field",
+                    actualValue: dictionary["field"])
+            }
+            
+            guard let code = dictionary["code"] as? String else {
+                throw JSONDecodeError.missingValue(
+                    key: "code",
+                    actualValue: dictionary["code"])
+            }
+            
+            self.resource = resource
+            self.field = field
+            self.code = code
+        }
     }
 }
